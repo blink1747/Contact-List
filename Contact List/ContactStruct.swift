66c90626f2c4a09120bb8c5379f7a7846008b325
Sujeet Poudel <sujeetpoudel.np@gmail.com>
2019-08-14 00:41:52 -0500

    Final Code

---
+++ Contact List/ContactStruct.swift
@@ -0,0 +1,15 @@
+//
+//  ContactStruct.swift
+//  Contact List
+//
+//  Created by IMCS on 8/13/19.
+//  Copyright © 2019 IMCS. All rights reserved.
+//
+
+import Foundation
+
+struct ContactStruct {
+    let givenName: String
+    let familyName: String
+    let number: String
+}
